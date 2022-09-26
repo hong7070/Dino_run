232fd28192e751b54e4e97f7fea18e5c7d444ec1
강재영 <jaeyoungkang@wirelessprv-10-194-35-99.near.illinois.edu>
2022-09-25 23:33:26 -0500

    kang

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ height_screen = 150
 display_surface = pygame.display.set_mode((width_screen, height_screen))    #display
 pygame.display.set_caption("Dino run")                                      #window 
 
-WHITE = (255,255,255)
+WHITE = (255,255,0)
 BLACK = (0,0,0)
 
 
@@ -22,6 +22,5 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     pygame.display.update()
-
 
 pygame.quit()
