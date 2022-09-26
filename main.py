5e1194c910b303bd75a7dfc1c94dd5e9b0e654a5
hojoon yoo <hojoony2@illinois.edu>
2022-09-25 23:33:52 -0500

    branch created

--- main.py
+++ main.py
@@ -2,16 +2,17 @@
 import pygame
 
 pygame.init()
+
 #screen_size_display = (width_screen, height_screen) = (600, 150)
 width_screen = 600
 height_screen = 150
 
-display_surface = pygame.display.set_mode((width_screen, height_screen))    #display 크기 설정
-pygame.display.set_caption("Dino run")                                      #window 이름 설정
+display_surface = pygame.display.set_mode(
+    (width_screen, height_screen))  # display
+pygame.display.set_caption("Dino run")  # window
 
-WHITE = (255,255,255)
-BLACK = (0,0,0)
-
+WHITE = (255, 255, 254)
+BLACK = (0, 0, 0)
 
 
 display_surface.fill(WHITE)
